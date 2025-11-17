db1fb4d3d19e71acacee11284292d47343c36c67
daysite <danieldevelop3@gmail.com>
2025-11-17 16:29:33 -0500

    Update Creador.js

--- plugins/Creador.js
+++ plugins/Creador.js
@@ -1,13 +1,13 @@
 const handler = async (msg, { conn }) => {
   const chatId = msg.key.remoteJid;
 
-  const ownerNumber = "15167096032@s.whatsapp.net"; // Número del dueño en formato WhatsApp
-  const ownerName = "Russell xz 🤖"; // Nombre que aparecerá en el contacto
+  const ownerNumber = "5493884539290@s.whatsapp.net"; // Número del dueño en formato WhatsApp
+  const ownerName = "Daniel"; // Nombre que aparecerá en el contacto
   const messageText = `📞 *Contacto del Creador:*\n
 Si tienes dudas, preguntas o sugerencias sobre el bot, puedes contactar a mi creador.
 
-📌 *Nombre:* Russell
-📌 *Número:* +1 (516) 709-6032
+📌 *Nombre:* Daniel
+📌 *Número:* +54 9 388 453-9290
 💬 *Mensaje directo:* Pulsa sobre el contacto y chatea con él.`;
 
   // 🧾 Enviar vCard del creador
